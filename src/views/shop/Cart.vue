f93b0171d30d896a1dd2c2543f75f004dbfc0826
xjqgo <zqgo@qq.com>
2021-08-16 15:31:08 +0800

    1-7 商家详情布局

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="cart">
     <div>
-      <img src="	http://www.dell-lee.com/imgs/vue3/basket.png" />
-      <span>购物车是空的</span>
+      <img src="http://www.dell-lee.com/imgs/vue3/basket.png" />
+      <div class="cart__icon">1</div>
+      <!-- <span class="cart__text">购物车是空的</span> -->
+      <span class="cart__text">合计：</span>
+      <span class="cart__price">￥128</span>
     </div>
     <div class="cart__jiesuan">去结算</div>
   </div>
@@ -13,8 +16,10 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "./style/vrirables.scss";
 .cart {
   display: flex;
+  position: relative;
   justify-content: space-between;
   align-items: center;
   position: absolute;
@@ -28,15 +33,34 @@ export default {};
       height: 0.26rem;
       margin: 0 0.32rem 0 0.24rem;
   }
-  span{
+  &__icon{
+      position: absolute;
+      width: .2rem;
+      height: .2rem;
+      border-radius: 50%;
+      background: $textColor-lightRed;
+      top: .02rem;
+      left: .4rem;
+      color: $textColor-white;
+      font-size: .16rem;
+      text-align: center;
+      transform: scale(.5);
+  }
+  &__text{
       position: relative;
       top: -0.07rem;
   }
-  .cart__jiesuan{
+  &__price{
+      font-size: .18rem;
+      color: $textColor-lightRed;
+      position: relative;
+      top: -0.05rem;
+  }
+  &__jiesuan{
       width: 0.98rem;
       line-height: 0.49rem;
       background: #4FB0F9;
-      color: white;
+      color: $textColor-white;
       text-align: center;
       font-size: 0.14rem;
   }
--- src/views/shop/Content.vue
+++ src/views/shop/Content.vue
@@ -181,7 +181,7 @@ export default {
         }
       }
       .price {
-        color: #e93b3b;
+        color: $textColor-lightRed;
         span {
           display: inline-block;
         }
