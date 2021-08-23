f3f8212ae2f0b920848e0ec14b853bfd82acb368
xjqgo <zqgo@qq.com>
2021-08-23 11:53:45 +0800

    1-9 创建cartList页面并添加底部nav路由

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "home" */ '../views/home/Home'),
   },
   {
+    path: '/cartList',
+    name: 'CartList',
+    component: () => import(/* webpackChunkName: "cartList" */ '../views/cartList/CartList'),
+  },
+  {
     path: '/shop/:id',
     name: 'Shop',
     component: () => import(/* webpackChunkName: "home" */ '../views/shop/Shop'),
--- src/views/home/Docker.vue
+++ src/views/home/Docker.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="docker">
-    <div v-for="(item,index) in list" :key="item.icon" :class="{docker__item:true,'docker__item docker__item--active':index===0}">
-      <span class="iconfont" v-html="item.icon"></span
-      ><span class="docker__item__title">{{item.title}}</span>
+    <div
+      v-for="(item, index) in list"
+      :key="item.icon"
+      :class="{
+        docker__item: true,
+        'docker__item docker__item--active': index === 0,
+      }"
+    >
+      <router-link :to="item.to">
+        <span class="iconfont" v-html="item.icon"></span
+        ><span class="docker__item__title">{{ item.title }}</span>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Docker',
-  setup () {
+  name: "Docker",
+  setup() {
     const list = [
-      { title: '首页', icon: '&#xe6e2;' },
-      { title: '购物车', icon: '&#xe6e6;' },
-      { title: '订单', icon: '&#xe643;' },
-      { title: '我的', icon: '&#xe782;' }
-    ]
+      { title: "首页", icon: "&#xe6e2;", to: { name: "Home" } },
+      { title: "购物车", icon: "&#xe6e6;", to: { name: "CartList" } },
+      { title: "订单", icon: "&#xe643;", to: { name: "Home" } },
+      { title: "我的", icon: "&#xe782;", to: { name: "Home" } },
+    ];
 
-    return { list }
-  }
-}
+    return { list };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -36,7 +45,6 @@ export default {
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  color: $content-fontcolor;
   border-top: 0.01rem solid $content-bgColor;
 }
 .docker__item {
@@ -55,8 +63,20 @@ export default {
     transform: scale(0.5);
     transform-origin: top;
   }
+  a {
+    text-decoration: none;
+    color: $content-fontcolor;
+    .iconfont{
+      color: $blank-fontcolor;
+    }
+  }
   &--active {
-    color: #1fa4fc;
+    a {
+      color: #1fa4fc;
+    .iconfont{
+      color: #1fa4fc;
+    }
+    }
   }
 }
 </style>
