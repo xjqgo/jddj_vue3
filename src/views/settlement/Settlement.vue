425460c8d1ed98affb5a1cfbb90744dde22a7cee
xjqgo <zqgo@qq.com>
2021-08-25 22:28:29 +0800

    1-10 完成收货地址布局

--- src/views/settlement/Settlement.vue
+++ src/views/settlement/Settlement.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="top">
-    <div class="bgcor" />
-    <div class="top__hander">
-      <div class="iconfont top__hander__back">&#xe64c;</div>
-      确认订单
-    </div>
-    <div class="top__receiver">
-      <div class="top__receiver__title">收货地址</div>
-      <div class="top__receiver__address">北京理工大学国防科技园2号楼10层</div>
-      <div class="top__receiver__info">
-        <span class="top__receiver__info__name">瑶妹（女士）</span>
-        <span class="top__receiver__info__name">18911024266</span>
+  <div class="wrapper">
+    <div class="top">
+      <div class="top__hander">
+        <div class="iconfont top__hander__back">&#xe64c;</div>
+        确认订单
       </div>
-      <div class="iconfont top__receiver__enter">&#xe64c;</div>
+      <div class="top__receiver">
+        <div class="top__receiver__title">收货地址</div>
+        <div class="top__receiver__address">
+          北京理工大学国防科技园2号楼10层
+        </div>
+        <div class="top__receiver__info">
+          <span class="top__receiver__info__name">瑶妹（女士）</span>
+          <span class="top__receiver__info__name">18911024266</span>
+        </div>
+        <div class="iconfont top__receiver__icon">&#xe64c;</div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,10 +25,57 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  background: #f8f8f8;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
 .top {
-    height: 1.9rem;
-    background-size: 100% 1.59rem;
-    background-image: linear-gradient(0deg, rgba(0,145,255,0.00) 4%, #0091FF 50%);
-    background-repeat: no-repeat;
+  height: 1.9rem;
+  background-size: 100% 1.59rem;
+  background-image: linear-gradient(0deg, rgba(0, 145, 255, 0) 4%, #0091ff 50%);
+  background-repeat: no-repeat;
+  padding: 0.26rem 0.18rem 0 0.18rem;
+  &__hander {
+    font-size: 0.16rem;
+    text-align: center;
+    color: #ffffff;
+    line-height: 0.22rem;
+    &__back {
+      position: absolute;
+      font-weight: 1000;
+    }
+  }
+  &__receiver {
+    position: relative;
+    margin-top: 0.22rem;
+    padding: 0.16rem;
+    background: #ffffff;
+    &__title {
+      font-size: 0.16rem;
+      font-weight: 700;
+      line-height: 0.22rem;
+    }
+    &__address {
+      margin: 0.14rem 0 0.06rem;
+      font-size: 0.14rem;
+      line-height: 0.2rem;
+      color: #333;
+    }
+    &__info {
+      line-height: 0.17rem;
+      color: #666;
+    }
+    &__icon {
+      transform: rotate(180deg);
+      position: absolute;
+      right: .16rem;
+      top: .5rem;
+      font-size: .16rem;
+    }
+  }
 }
 </style>
