e40da05aa6116a1115fd77e6503bbc982f3d25c5
xjqgo <zqgo@qq.com>
2021-08-28 00:38:50 +0800

    1-10 现实mask显示逻辑

--- src/views/settlement/Settlement.vue
+++ src/views/settlement/Settlement.vue
@@ -1,70 +1,27 @@
 <template>
   <div class="wrapper">
-    <div class="mask" v-show="1">
-      <div class="msgbox">
-        <div class="msgbox__title">确认要离开收银台？</div>
-        <div class="msgbox__text">请尽快完成支付，否则将被取消</div>
-        <div class="msgbox__button">
-          <span @click="btnRquest(false)" class="msgbox__button__no">取消订单</span>
-          <span @click="btnRquest(true)" class="msgbox__button__yes">确认支付</span>
-        </div>
-      </div>
-    </div>
     <Top />
     <Shop v-for="item in { cartShop }" :key="item" :item="item" />
     <Bottom-order />
-    <Toast v-show="toastData.showToast" :msg="toastData.msg" />
   </div>
 </template>
 
 <script>
 import Shop from "../cartList/Shop.vue";
-import { cartEffect } from "../../effects/cartEffect";
-import { useRoute } from "vue-router";
 import Top from "./topArea.vue";
 import BottomOrder from "./bottomOrder.vue";
-import { post } from "../../util/request";
-import Toast, { showToastEffect } from "../../components/Toast.vue";
+import { cartEffect } from "../../effects/cartEffect";
+import { useRoute } from "vue-router";
 
 
 export default {
-  components: { Shop, Top, BottomOrder,Toast },
+  components: { Shop, Top, BottomOrder },
   setup() {
-    // const {btnNo,btnYes} = buttonEffect();
     const route = useRoute();
     const shopId = parseInt(route.params.id);
     const { cartShop } = cartEffect(shopId);
-    const { toastData, showToast } = showToastEffect();
 
-    //提交订单
-    const btnRquest = async (isCanceled) => {
-      console.log("yes");
-      const products = [];
-      for (const i in cartShop.productList) {
-        if (Object.hasOwnProperty.call(cartShop.productList, i)) {
-          const element = cartShop.productList[i];
-          products.push({id:parseInt(element._id),num:element.count})
-        }
-      }
-      try {
-        const result = await post("/api/order", {
-          addressId: 1,
-          shopId,
-          shopName: cartShop.shopName,
-          isCanceled,
-          products,
-        });
-        console.log("返回结果", result);
-        if (result.errno === 0) {
-          showToast("登陆成功");
-        } else {
-          showToast("登陆失败");
-        }
-      } catch (e) {
-        showToast("请求失败");
-      }
-    };
-    return { toastData,cartShop,btnRquest };
+    return {cartShop}
   },
 };
 </script>
@@ -81,59 +38,5 @@ export default {
   padding-top: 1.9rem;
   padding-bottom: 0.3rem;
   overflow-y: auto;
-}
-.mask {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba($color: #000000, $alpha: 0.5);
-  z-index: 111;
-}
-.msgbox {
-  width: 3rem;
-  height: 1.57rem;
-  // padding: .24rem 0;
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
-  background: #fff;
-  position: relative;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  &__title {
-    line-height: 0.25rem;
-    font-weight: 600;
-    font-size: 18px;
-    color: #333333;
-  }
-  &__text {
-    font-size: 0.14rem;
-    color: #666;
-    line-height: 0.2rem;
-    margin: 0.08rem 0 0.3rem;
-  }
-  &__button {
-    span {
-      border: 1px solid #4fb0f9;
-      border-radius: 16px;
-      font-size: 0.14rem;
-      text-align: center;
-      width: 0.78rem;
-      line-height: 0.3rem;
-      display: inline-block;
-    }
-    &__no {
-      margin-right: 0.24rem;
-      color: #0091ff;
-    }
-    &__yes {
-      background: #4fb0f9;
-      color: #fff;
-    }
-  }
 }
 </style>
--- src/views/settlement/bottomOrder.vue
+++ src/views/settlement/bottomOrder.vue
@@ -1,19 +1,34 @@
 <template>
+    <div @click.self="showMask=false" class="mask" v-show="showMask">
+      <div class="msgbox">
+        <div class="msgbox__title">确认要离开收银台？</div>
+        <div class="msgbox__text">请尽快完成支付，否则将被取消</div>
+        <div class="msgbox__button">
+          <span @click="btnRquest(false)" class="msgbox__button__no">取消订单</span>
+          <span @click="btnRquest(true)" class="msgbox__button__yes">确认支付</span>
+        </div>
+      </div>
+    </div>
   <div class="settlement">
     <div class="settlement__total">
       <span class="settlement__total__title">付款金额</span>
       <span class="settlement__total__price"> ￥{{ total }}</span>
     </div>
-    <div class="settlement__btn">提交订单</div>
+    <div @click="showMask=true" class="settlement__btn">提交订单</div>
   </div>
+    <Toast v-show="toastData.showToast" :msg="toastData.msg" />
 </template>
 
 <script>
 import { cartEffect } from "../../effects/cartEffect";
 import { useRoute } from "vue-router";
-import { computed } from "@vue/runtime-core";
-export default {
-  setup() {
+import { computed, ref } from "@vue/runtime-core";
+import Toast, { showToastEffect } from "../../components/Toast.vue";
+import { post } from "../../util/request";
+
+// 订单相关
+const orderEffect = () => {
+   
     let { cartList } = cartEffect();
     const route = useRoute();
     cartList = { info: cartList[route.params.id] };
@@ -27,6 +42,56 @@ export default {
     });
 
     return { cartList, total };
+}
+
+// 
+const maskEffect = () => {
+   
+    const route = useRoute();
+    const shopId = parseInt(route.params.id);
+    const { cartShop } = cartEffect(shopId);
+    const { toastData, showToast } = showToastEffect();
+console.log(123,cartShop);
+    //提交订单
+    const btnRquest = async (isCanceled) => {
+      console.log("yes");
+      const products = [];
+      for (const i in cartShop.productList) {
+        if (Object.hasOwnProperty.call(cartShop.productList, i)) {
+          const element = cartShop.productList[i];
+          products.push({id:parseInt(element._id),num:element.count})
+        }
+      }
+      try {
+        const result = await post("/api/order", {
+          addressId: 1,
+          shopId,
+          shopName: cartShop.shopName,
+          isCanceled,
+          products,
+        });
+        console.log("返回结果", result);
+        if (result.errno === 0) {
+          showToast("登陆成功");
+        } else {
+          showToast("登陆失败");
+        }
+      } catch (e) {
+        showToast("请求失败");
+      }
+    };
+    return { toastData,cartShop,btnRquest };
+}
+
+export default {
+  components:{Toast},
+  setup() {
+    const showMask = ref(false);
+    const {  cartList, total  } = orderEffect();
+    console.log(1);
+    const { toastData,cartShop,btnRquest } = maskEffect();
+
+    return { cartList, total,showMask,toastData,cartShop,btnRquest};
   },
 };
 </script>
@@ -57,6 +122,60 @@ export default {
     color: $textColor-white;
     font-size: 0.14rem;
     background: rgba($color: $bgColor-lightBlue, $alpha: 0.7);
+  }
+}
+.mask {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba($color: #000000, $alpha: 0.5);
+  z-index: 111;
+}
+.msgbox {
+  width: 3rem;
+  height: 1.57rem;
+  // padding: .24rem 0;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  &__title {
+    line-height: 0.25rem;
+    font-weight: 600;
+    font-size: 18px;
+    color: #333333;
+  }
+  &__text {
+    font-size: 0.14rem;
+    color: #666;
+    line-height: 0.2rem;
+    margin: 0.08rem 0 0.3rem;
+  }
+  &__button {
+    span {
+      border: 1px solid #4fb0f9;
+      border-radius: 16px;
+      font-size: 0.14rem;
+      text-align: center;
+      width: 0.78rem;
+      line-height: 0.3rem;
+      display: inline-block;
+    }
+    &__no {
+      margin-right: 0.24rem;
+      color: #0091ff;
+    }
+    &__yes {
+      background: #4fb0f9;
+      color: #fff;
+    }
   }
 }
 </style>
