7e796385c93965b2ad2cddd5532d421360d4b3ee
xjqgo <zqgo@qq.com>
2021-08-30 15:12:21 +0800

    1-12 收货地址布局

--- src/views/userAddress/UserAddress.vue
+++ src/views/userAddress/UserAddress.vue
@@ -1,15 +1,51 @@
 <template>
-    <div>
-        useraddres
+  <div class="wrapper">
+    <div class="wrapper__hander">
+      <div class="wrapper__hander__back iconfont">&#xe64c;</div>
+      <div class="wrapper__hander__title">管理收货地址</div>
+      <div class="wrapper__hander__add">新增</div>
     </div>
+    <div class="wrapper__address">
+      <div class="wrapper__address__title">我的收货地址</div>
+      <Info />
+    </div>
+  </div>
 </template>
 
 <script>
+import Info from "./infoList.vue";
+
 export default {
-    
-}
+  components: { Info },
+  setup() {},
+};
 </script>
 
 <style lang="scss" scoped>
+@import "./style/mixins.scss";
 
+.wrapper {
+  @include wrapper;
+  bottom: 0;
+  padding: 0.76rem 0 .3rem;
+  &__hander {
+    @include hander;
+    display: flex;
+    justify-content: space-between;
+    &__back{
+        color: #B6B6B6;
+        font-size: .2rem;
+        position: relative;
+        top: -.03rem;
+    }
+  }
+  &__address {
+    &__title {
+      margin: 0 0.18rem 0.12rem;
+      font-size: 14px;
+      color: #333333;
+      line-height: .2rem;
+    }
+  }
+}
 </style>
---
+++ src/views/userAddress/infoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="info">
+    <div class="info__name">
+      小慕<span class="info__name__phome">18911023261</span>
+    </div>
+    <div class="info__address">
+      北京市海淀区西三环北路 2号院 北京理工大学 国防科技园2号楼 10层
+    </div>
+    <div class="info__edit iconfont">&#xe64c;</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style lang="scss" scoped>
+.info {
+  margin: 0 0.18rem 0.16rem 0.18rem;
+  padding: 0.16rem;
+  background: #fff;
+  border-radius: 4px;
+  font-size: 14px;
+  position: relative;
+  &__name {
+    line-height: 0.2rem;
+    margin-bottom: 0.08rem;
+    color: #999999;
+    &__phome {
+      margin-left: 0.66rem;
+    }
+  }
+  &__address {
+    color: #333;
+    margin-right: 0.63rem;
+  }
+  &__edit {
+    position: absolute;
+    right: .16rem;
+    top: .41rem;
+    transform: rotate(180deg);
+  }
+}
+</style>
