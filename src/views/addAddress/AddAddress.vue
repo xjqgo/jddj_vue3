3b5080add3d4c846782ab673285a6259ba7e205b
xjqgo <zqgo@qq.com>
2021-08-31 14:48:42 +0800

    1-12 getRequest

--- src/views/addAddress/AddAddress.vue
+++ src/views/addAddress/AddAddress.vue
@@ -3,70 +3,68 @@
     <div class="wrapper__hander">
       <Back />
       <div class="wrapper__hander__title">新建收货地址</div>
-      <div class="wrapper__hander__add">保存</div>
+      <div class="wrapper__hander__add" @click="getRequest()">保存</div>
     </div>
     <div class="wrapper__info">
       <div class="wrapper__info__item">
-        所在城市：<input v-model="rquestData.city" placeholder="如北京" />
+        所在城市：<input v-model="requestData.city" placeholder="如北京" />
       </div>
       <div class="wrapper__info__item">
         小区/大厦/学校：<input
-          v-model="rquestData.department"
+          v-model="requestData.department"
           placeholder="如理工大学国防科技园"
         />
       </div>
       <div class="wrapper__info__item">
         楼号-门牌号：<input
-          v-model="rquestData.houseNumber"
+          v-model="requestData.houseNumber"
           placeholder="如A号楼B层"
         />
       </div>
       <div class="wrapper__info__item">
         收货人：<input
-          v-model="rquestData.name"
+          v-model="requestData.name"
           placeholder="请填写收货人的姓名"
         />
       </div>
       <div class="wrapper__info__item zh">
         联系电话：<input
-          v-model="rquestData.phone"
+          v-model="requestData.phone"
           placeholder="请填写收货手机号"
         />
       </div>
     </div>
-    {{ rquestData.city }}
+    {{ requestData.city }}
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
-import { get } from "../../util/request";
+import { reactive } from "@vue/reactivity";
+import { post } from "../../util/request";
 import Back from "../../components/back.vue";
 //
 const getAddressEffect = () => {
-  const data = ref([]);
-  const getRquest = async () => {
+    const requestData = reactive({city: "", department: "", houseNumber: "", name: "", phone: ""});
+  const getRequest = async () => {
     try {
-      const result = await get(`/api/user/address`);
+      const result = await post(`/api/user/address`,{data:requestData});
       console.log("返回结果address", result);
       if (result?.errno === 0 && result?.data) {
-        data.value = result.data;
+          console.log('ok');
       }
     } catch (e) {
       alert("请求失败:" + e);
     }
   };
-  getRquest();
-  return { data };
+  return { requestData,getRequest };
 };
 
 export default {
   components: { Back },
   setup() {
-    const { data } = getAddressEffect();
-    const rquestData = reactive([]);
+    const { requestData,getRequest } = getAddressEffect();
 
-    return { data, rquestData };
+    return {  requestData,getRequest };
   },
 };
 </script>
