2984c86ef3e9cea2fed84e0233952adff5efdfd4
xjqgo <zqgo@qq.com>
2021-09-02 10:23:32 +0800

    1-13 退出登陆

--- src/views/person/Person.vue
+++ src/views/person/Person.vue
@@ -2,6 +2,7 @@
     <div class="wrapper">
         <Top-user-info />
         <Item />
+        <div class="wrapper__logout" @click="handlerLogout">退出登陆</div>
         <Docker :currentIndex="3" />
     </div>
 </template>
@@ -9,11 +10,18 @@
 <script>
 import Docker from '../../components/Docker.vue'
 import TopUserInfo from './TopUserInfo.vue'
-import Item from './Item.vue'
+import Item from './Item'
+import { useRouter } from 'vue-router'
 export default {
     components:{Docker,TopUserInfo,Item},
     setup() {
+        const router = useRouter();
+        const handlerLogout = () => {
+            delete localStorage.isLogin
+            router.push({name:'Login'})
+        };
         
+        return {handlerLogout}
     }
 }
 </script>
@@ -22,5 +30,13 @@ export default {
 @import "./style/mixins.scss";
 .wrapper{
     @include wrapper(0);
+    &__logout{
+        margin: .18rem;
+        color: $textColor-white;
+        text-align: center;
+        line-height: .32rem;
+        border-radius: .04rem;
+        background: $bgColor-lightBlue;
+    }
 }
 </style>
