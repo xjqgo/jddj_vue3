0b71a11230d5b6c6b1e84b0dd41aaaabf82b2c19
xjqgo <zqgo@qq.com>
2021-09-08 11:54:49 +0800

    1-15 订单提交成功提示框

--- src/views/order/bottomOrder.vue
+++ src/views/order/bottomOrder.vue
@@ -1,6 +1,6 @@
 <template>
   <div @click.self="showMask = false" class="mask" v-show="showMask">
-    <div class="msgbox">
+    <div class="msgbox" v-show="msgboxNum == 1">
       <div class="msgbox__title">确认要离开收银台？</div>
       <div class="msgbox__text">请尽快完成支付，否则将被取消</div>
       <div class="msgbox__button">
@@ -11,6 +11,11 @@
           >确认支付</span
         >
       </div>
+    </div>
+    <div class="success" v-show="msgboxNum == 2">
+      <div class="success__icon-cancel iconfont">&#xe65f;</div>
+      <div class="success__icon-yes iconfont">&#xe80f;</div>
+      <div class="success__title">支付成功，等待配送</div>
     </div>
   </div>
   <div class="order">
@@ -48,7 +53,7 @@ const orderEffect = () => {
 };
 
 // 蒙层
-const maskEffect = (showMask) => {
+const maskEffect = (showMask, msgboxNum) => {
   const { cartShop, shopId, changeCartItem } = cartEffect();
   const { toastData, showToast } = showToastEffect();
   const router = useRouter();
@@ -74,15 +79,15 @@ const maskEffect = (showMask) => {
       console.log("返回结果", result);
       if (result.errno === 0) {
         if (isCanceled) {
-          showToast("订单提交成功");
+          msgboxNum.value = 2;
           setTimeout(() => {
             changeCartItem(shopId, { _id: "clearOrder" });
             router.push({ name: "OrderList" });
           }, 2000);
         } else {
+          showMask.value = false;
           showToast("已取消");
         }
-        showMask.value = false;
       } else {
         showToast("登陆失败");
       }
@@ -96,11 +101,20 @@ const maskEffect = (showMask) => {
 export default {
   components: { Toast },
   setup() {
-    const showMask = ref(false);
+    const showMask = ref(true);
+    const msgboxNum = ref(1);
     const { cartList, orderPrice } = orderEffect();
-    const { toastData, cartShop, btnRquest } = maskEffect(showMask);
+    const { toastData, cartShop, btnRquest } = maskEffect(showMask, msgboxNum);
 
-    return { cartList, orderPrice, showMask, toastData, cartShop, btnRquest };
+    return {
+      cartList,
+      orderPrice,
+      showMask,
+      msgboxNum,
+      toastData,
+      cartShop,
+      btnRquest,
+    };
   },
 };
 </script>
@@ -141,17 +155,18 @@ export default {
   bottom: 0;
   background: rgba($color: $blank-fontcolor, $alpha: 0.5);
   z-index: 111;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
 }
 .msgbox {
-  width: 3rem;
+  margin: 0 0.37rem;
   height: 1.57rem;
-  // padding: .24rem 0;
   display: flex;
   flex-flow: column;
   justify-content: center;
   align-items: center;
   background: $bgColor-white;
-  @include center;
   &__title {
     line-height: 0.25rem;
     font-weight: 600;
@@ -182,6 +197,35 @@ export default {
       background: $dorder-button;
       color: $textColor-white;
     }
+  }
+}
+.success {
+  background: #fff;
+  margin: 0 0.37rem;
+  padding: 0.12rem;
+  position: relative;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  &__icon-cancel {
+    font-size: 0.13rem;
+    text-align: right;
+    align-self: flex-end;
+    color: #666666;
+  }
+  &__icon-yes {
+    font-size: 0.28rem;
+    transform: scaleX(1.5);
+    font-weight: 1000;
+    color: #333;
+    margin-top: 0.16rem;
+  }
+  &__title {
+    font-size: 0.18rem;
+    font-weight: 600;
+    color: #333;
+    line-height: 0.25rem;
+    margin: 0.24rem 0 0.2rem;
   }
 }
 </style>
