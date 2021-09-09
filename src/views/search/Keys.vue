cbf084cb7c3fa781e932c1d41f9ccdf59436a311
xjqgo <zqgo@qq.com>
2021-09-09 21:01:11 +0800

    1-15 搜索页面部分

---
+++ src/views/search/Keys.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="keys">
+    <div class="keys__hander">
+      <div class="keys__hander__name">{{ name }}</div>
+      <div class="keys__hander__clear" v-if="clear" @click="handlerClear">清除搜索历史</div>
+    </div>
+    <div class="keys__list">
+      <router-link :to="`searchRes?key=${item}`" class="keys__list__item" v-for="item in arr" :key="item">{{item}}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "clear", "arr"],
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./style/mixins.scss";
+
+.keys {
+  margin-top: 0.24rem;
+      color: #333;
+  &__hander {
+    display: flex;
+    justify-content: space-between;
+    &__name {
+      font-size: 0.16rem;
+      font-weight: 600;
+      margin-bottom: .12rem;
+    }
+    &__clear {
+      font-size: 0.14rem;
+    }
+  }
+  &__list{
+      background: #fff;
+      margin-right: -.1rem;
+      &__item{
+          display: inline-block;
+          font-size: .14rem;
+          line-height: .16rem;
+          color: #666;
+          padding: .08rem .1rem;
+          margin: 0 .1rem .12rem 0;
+          background: #F5F5F5;
+          border-radius: .02rem;
+      }
+  }
+}
+</style>
--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,18 +1,90 @@
 <template>
-    <div class="wrapper">
-        search
+  <div class="wrapper">
+    <div class="search">
+      <div to="/search" class="search__input">
+        <span class="iconfont search__input__icon">&#xe60c;</span>
+        <input class="search__input__text" placeholder="尖椒肉丝" />
+      </div>
+      <router-link to>
+        <div class="iconfont search__cancel" @click="$router.back()">取消</div>
+      </router-link>
     </div>
+    <Keys name="搜索历史" :arr="keysHistory" :clear="true" />
+    <Keys name="热门搜索" :arr="keysHot" />
+  </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
+import Keys from "./Keys.vue";
 export default {
-    
-}
+  components: { Keys },
+  setup() {
+    const keysHistory = ref([
+      "尖椒肉丝",
+      "鲜花",
+      "山姆会员商店",
+      "新鲜水果.",
+      "生日鲜花",
+      "香槟玫瑰",
+      "酸奶",
+      "牛奶",
+    ]);
+    const keysHot = ref([
+      "C罗",
+      "梅西",
+      "内马尔",
+      "武磊",
+      "丁俊晖",
+      "香槟玫瑰",
+      "酸奶",
+      "牛奶",
+    ]);
+
+    return { keysHistory, keysHot };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "./style/mixins.scss";
-.wrapper{
-    @include wrapper;
+.wrapper {
+  padding: 0.16rem 0.18rem 0;
+}
+.search {
+  display: flex;
+  white-space: nowrap;
+  height: 0.32rem;
+  .search__input {
+    line-height: 0.32rem;
+    background: #f5f5f5;
+    border-radius: 0.16rem;
+    color: #b7b7b7;
+    width: 100%;
+    font-size: 0.14rem;
+    &__icon {
+      padding: 0 0.05rem 0 0.16rem;
+    }
+    &__text {
+      width: 83%;
+      border: 0;
+      outline: none; // 去除选中状态边框
+      background: inherit;
+      color: #333;
+      position: relative;
+      top: -0.02rem;
+    }
+    &__text::-webkit-input-placeholder {
+      //占位符颜色
+      color: #333;
+      font-size: 0.14rem;
+    }
+  }
+  &__cancel {
+    font-size: 0.16rem;
+    line-height: 0.32rem;
+    color: #333;
+    margin-left: 0.12rem;
+  }
 }
 </style>
